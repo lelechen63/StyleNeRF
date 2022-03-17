9f71f6ab1c55137d2d4022fa63725f0d52a3b132
Lele Chen <lelechen@Innopeaks-MacBook-Pro-2.local>
2022-03-17 12:02:29 -0700

    g

--- training/networks.py
+++ training/networks.py
@@ -1090,7 +1090,7 @@ class Generator(torch.nn.Module):
 
     def get_final_output_withws(self, *args, **kwargs):
         img = self.forward(*args, **kwargs)
-        print (type(img))
+        print (type(img), '=+++++++')
         if isinstance(img, list):
             return img[-1]
         elif isinstance(img, dict):
