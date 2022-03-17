571d537d5cf3da65938692e51dee88eef2f40aff
Lele Chen <lelechen@Innopeaks-MacBook-Pro-2.local>
2022-03-16 21:46:28 -0700

    g

--- apps/inversion.py
+++ apps/inversion.py
@@ -147,8 +147,6 @@ def main(
             kwargs['latent_codes'][3].requires_grad = True
             opt_weights += [{'params': kwargs['latent_codes'][3]}]
 
-    print (kwargs.keys(), '++++++')
-
     optimizer = optim.Adam(opt_weights, lr=0.01, betas=(0.9,0.999), eps=1e-8)
     
     print("Start...")
