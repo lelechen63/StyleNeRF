afa0fa71bac48e5e8cdb401d607afaab1ac25650
Lele Chen <lelechen@Innopeaks-MacBook-Pro-2.local>
2022-03-16 17:31:02 -0700

    g

--- apps/inversion.py
+++ apps/inversion.py
@@ -149,7 +149,7 @@ def main(
 
     print (kwargs.keys(), '++++++')
 
-    optimizer = optim.Adam(opt_weights, lr=0.1, betas=(0.9,0.999), eps=1e-8)
+    optimizer = optim.Adam(opt_weights, lr=0.01, betas=(0.9,0.999), eps=1e-8)
     
     print("Start...")
     loss_list = []
