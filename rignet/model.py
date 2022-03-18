062a64ced2b1e03ef2c17e14df7793be85d23007
Lele Chen <lelechen@Innopeaks-MacBook-Pro-2.local>
2022-03-18 12:03:44 -0700

    g

--- rignet/model.py
+++ rignet/model.py
@@ -277,6 +277,7 @@ class RigNerft(nn.Module):
     
     def rig(self,w, p):
         shapecode, expcode, albedocode, litcode = p[0], p[1],p[2], p[3]
+        litcode = litcode.view(-1, 27)
         l_w = self.LatentEncoder(w)
        
         l_p = self.ParamEncoder(torch.cat([shapecode, expcode, albedocode, litcode], axis = 1))
