7b9ae07c2c938e4cbe0f6d704648b9ea2bc16ffc
Lele Chen <lelechen@Innopeaks-MacBook-Pro-2.local>
2022-03-18 10:36:09 -0700

    g

--- rignet/latent2code.py
+++ rignet/latent2code.py
@@ -141,6 +141,12 @@ class Latent2CodeModule():
                 ('genlmark', genlmark )
                 ])
                 if self.opt.supervision == 'render':
+                    gtimage = vis_tensor(image_tensor= rbatch['gt_image'], 
+                                        image_path = batch['image_path'][0] ,
+                                        device = self.device
+                                         )
+
+                    visuals.append(('gtimage', gtimage))
                     
                 self.visualizer.display_current_results(visuals, epoch, self.opt.save_step) 
                 torch.save(self.latent2code.module.Latent2fea.state_dict(), self.opt.Latent2ShapeExpCode_weight)
