0881ea6a1b8641afacadfa979e970266d6526e09
Lele Chen <lelechen@Innopeaks-MacBook-Pro-2.local>
2022-03-24 11:56:52 -0700

    gg

--- training/stylenerf.py
+++ training/stylenerf.py
@@ -966,7 +966,9 @@ class VolumeRenderer(object):
         H.tgt_res, H.n_points = int(math.sqrt(vol_pixels.size(1))), pixels.size(1)
         nerf_input_cams = self.C.get_origin_direction(pixels, camera_matrices)
         
-        print (nerf_input_cams.shape, '++++')
+        for kk in nerf_input_cams:
+            print (kk.shape)
+        print ( '++++')
 
         # set up an frozen camera for background if necessary
         if ('freeze_bg' in H.render_option) and (bg_nerf is not None):
