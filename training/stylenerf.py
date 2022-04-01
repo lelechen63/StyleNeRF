b848913908a6305cd178751788024b67d9308954
Lele Chen <lelechen@Innopeaks-MacBook-Pro-2.local>
2022-03-31 18:01:23 -0700

    gg

--- training/stylenerf.py
+++ training/stylenerf.py
@@ -831,6 +831,7 @@ class VolumeRenderer(object):
         # sample points
         di = torch.linspace(0., 1., steps=H.n_steps).to(H.device)
         print (di.shape)
+        print (H.batch_size, H.n_points)
         di = repeat(di, 's -> b n s', b=H.batch_size, n=H.n_points)
         print (di.shape, '++++++----+++-----+++')
         if (H.training and (not H.get('disable_noise', False))) or H.get('force_noise', False):
