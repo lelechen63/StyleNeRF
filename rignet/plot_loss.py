384952ee71ec6c7daf3ae65e6e43af15d30bfe51
Lele Chen <lelechen@Innopeaks-MacBook-Pro-2.local>
2022-04-06 22:57:08 -0700

    gg

--- rignet/plot_loss.py
+++ rignet/plot_loss.py
@@ -45,6 +45,6 @@ reader.close()
 
 axis = [i for i in range(len(loss_l2_v))]
 print (axis)
-plt.plot(axis, loss_l2_v, 'r--', axis, loss_p_v, 'b--',axis, loss_land_v, 'g--', axis, loss_l2_w, 'r*', axis, loss_p_w, 'b*',axis, loss_land_w, 'g*')
+plt.plot(axis, loss_l2_v, 'r--', axis, loss_p_v, 'b--',axis, loss_land_v, 'g--', axis, loss_l2_w)# 'r*', axis, loss_p_w, 'b*',axis, loss_land_w, 'g*')
 plt.show()
 plt.savefig('./gg.png')
