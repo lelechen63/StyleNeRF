b6d83de24a05dc63e18a43384a962b050a971d21
Lele Chen <lelechen@Innopeaks-MacBook-Pro-2.local>
2022-04-06 22:53:53 -0700

    gg

--- rignet/plot_loss.py
+++ rignet/plot_loss.py
@@ -8,31 +8,53 @@ reader = open(losstxt)
 l = reader.readline()
 l = reader.readline()
 ss = 0
-loss_land = []
-loss_tex = []
+loss_l2_v = []
+loss_p_v = []
+loss_land_v = []
+
+loss_l2_w = []
+loss_p_w = []
+loss_land_w = []
+
 axis =[]
 while l:
     tmp = l[:-1].split(' ')
     try:
-        print (l)
-        print (tmp)
-        l_land = tmp[-4]
-        l_tex =tmp[-2]
-        loss_land.append(float(l_land))
-        loss_tex.append(float(l_tex))
+        l2_v = tmp[-4]
+        p_v =tmp[-2]
+        land_v = tmp[-6]
+        p_v =tmp[-2]
+
+        l2_v = tmp[-10]
+        p_w =tmp[-8]
+        land_v = tmp[-12]
+
+
+        loss_l2_v.append(float(l2_v))
+        loss_l2_w.append(float(l2_w))
+
+        loss_p_v.append(float(p_v))
+        loss_p_w.append(float(p_w))
+
+        loss_land_w.append(float(land_w))
+        loss_land_v.append(float(land_v))
     except:
-        loss_land = []
-        loss_tex = []
-        
+        loss_l2_v = []
+        loss_p_v = []
+        loss_land_v = []
+
+        loss_l2_w = []
+        loss_p_w = []
+        loss_land_w = []
     # ss += 1
     # if ss == 1000:
     #     break
     l = reader.readline()
 reader.close()
-loss_tex = loss_tex[100:]
-loss_land = loss_land[100:]
+# loss_tex = loss_tex[100:]
+# loss_land = loss_land[100:]
 
-axis = [i for i in range(len(loss_tex))]
-plt.plot(axis, loss_land, 'r--', axis, loss_tex, 'b--')
+axis = [i for i in range(len(loss_l2_v))]
+plt.plot(axis, loss_l2_v, 'r--', axis, loss_p_v, 'b--',axis, loss_land_v, 'g--', axis, loss_l2_w, 'r*', axis, loss_p_w, 'b*',axis, loss_land_w, 'g*')
 plt.show()
 plt.savefig('./gg.png')
