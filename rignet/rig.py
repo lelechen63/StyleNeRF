e3c94e232703e76a1d5a24ece6f8cc2fc8cf2f91
Lele Chen <lelechen@Innopeaks-MacBook-Pro-2.local>
2022-04-07 22:20:38 -0700

    gg

--- rignet/rig.py
+++ rignet/rig.py
@@ -112,23 +112,23 @@ class RigModule():
                 # close to w
                 # losses['landmark_w_'] =  util.l2_distance(landmark_w_[:, 17:, :2], batch[1]['gt_landmark'][:, 17:, :2]) * self.flame_config.w_lmks
                 # losses['photometric_texture_w_'] = MSE_Loss( batch[1]['img_mask'] * render_img_w_,  batch[1]['img_mask'] * batch[1]['gt_image']) * self.flame_config.w_pho
-                losses['photometric_texture_w_'] = MSE_Loss( render_img_w_, return_list['recons_images_w'].detach() ) * self.flame_config.w_pho
+                losses['photometric_texture_w_'] = MSE_Loss( render_img_w_, return_list['recons_images_w'].detach().float() ) * self.flame_config.w_pho
                 
                 assert render_img_w_.shape[-1] == 256
                 render_w_features = perceptual_net(render_img_w_)
-                w_features = perceptual_net(return_list['recons_images_w'].detach())
+                w_features = perceptual_net(return_list['recons_images_w'].detach().float())
                 
                 losses['percepture_w']  = caluclate_percepture_loss( render_w_features, w_features, MSE_Loss) * self.opt.lambda_percep
                 
                 # close to v
                 # losses['landmark_v_']  = util.l2_distance(landmark_v_[:, 17:, :2], batch[0]['gt_landmark'][:, 17:, :2]) * self.flame_config.w_lmks
                 # losses['photometric_texture_v_'] = MSE_Loss( batch[0]['img_mask'] * render_img_v_,  batch[0]['img_mask'] * batch[0]['gt_image'] ) * self.flame_config.w_pho
-                losses['photometric_texture_v_'] = MSE_Loss(  render_img_v_,  return_list['recons_images_w'].detach() ) * self.flame_config.w_pho
+                losses['photometric_texture_v_'] = MSE_Loss(  render_img_v_,  return_list['recons_images_w'].detach().float() ) * self.flame_config.w_pho
 
                 # render_v_features = perceptual_net(batch[0]['img_mask'] *render_img_v_)
                 render_v_features = perceptual_net(render_img_v_)
-                v_features = perceptual_net( return_list['recons_images_w'].detach())
-                
+                v_features = perceptual_net( return_list['recons_images_w'].detach().float())
+
                 losses['percepture_v']  = caluclate_percepture_loss( render_v_features, v_features, MSE_Loss) * self.opt.lambda_percep
 
                 loss = losses['w_same'] + \
