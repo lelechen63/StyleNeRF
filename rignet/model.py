f0c18a070ee0a9bc756444cf00854039e9c872f5
Lele Chen <lelechen@Innopeaks-MacBook-Pro-2.local>
2022-04-22 14:50:33 -0700

    gg

--- rignet/model.py
+++ rignet/model.py
@@ -388,6 +388,7 @@ class RigNerft(nn.Module):
         landmark_w_, render_img_w_ = self.flame_render(p_w_, pose_w, cam_w)
         landmark_v_, render_img_v_ = self.flame_render(p_v_, pose_v, cam_v)
         return_list = {}
+        return_list['choice'] = choice
 
         if flameshape_v != None:
             landmark_same, render_img_same = self.flame_render(p_w_same, pose_w, cam_w)
